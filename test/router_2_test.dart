e23f9f3b3ede47b8db44f4ceb25d84ba41c73050
Gavin Zhao <git@gzgz.dev>
2022-07-21 13:32:15 -0400

    Separate three different Mocks into three files
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

---
+++ test/router_2_test.dart
@@ -0,0 +1,72 @@
+import 'package:ngdart/di.dart';
+import 'package:ngpageloader/html.dart';
+import 'package:ngrouter/ngrouter.dart';
+import 'package:ngrouter/src/router/router_impl.dart';
+import 'package:ngtest/angular_test.dart';
+import 'package:test/test.dart';
+import 'package:mockito/annotations.dart';
+
+import 'package:mockbug/app_component.dart';
+import 'package:mockbug/app_component.template.dart' as ng;
+
+import 'app_po.dart';
+
+@GenerateMocks(
+  [],
+  customMocks: [
+    MockSpec<RouterImpl>(
+      as: #MockRouter,
+      returnNullOnMissingStub: true,
+    ),
+  ],
+)
+import 'router_2_test.mocks.dart';
+
+import 'router_test.template.dart' as self;
+
+late NgTestFixture<AppComponent> fixture;
+late AppPo po;
+
+@GenerateInjector([
+  ValueProvider.forToken(appBaseHref, '/'),
+  routerProviders,
+  ClassProvider(Router, useClass: MockRouter),
+])
+final InjectorFactory rootInjector = self.rootInjector$Injector;
+
+void main() {
+  final injector = InjectorProbe(rootInjector);
+  final testBed = NgTestBed<AppComponent>(
+    ng.AppComponentNgFactory,
+    rootInjector: injector.factory,
+  );
+
+  setUp(() async {
+    fixture = await testBed.create();
+    final context =
+        HtmlPageLoaderElement.createFromElement(fixture.rootElement);
+    po = AppPo.create(context);
+  });
+
+  test('the default title should be foo', () async {
+    expect(po.title, 'Foo');
+  });
+
+  test('router is not called', () async {});
+
+  tearDown(disposeAnyRunningTest);
+}
+
+class InjectorProbe {
+  InjectorFactory _parent;
+  Injector? _injector;
+
+  InjectorProbe(this._parent);
+
+  InjectorFactory get factory => _factory;
+  Injector? get injector => _injector;
+
+  // Injector _factory([Injector? parent]) => _injector = _parent(parent);
+  Injector _factory(Injector parent) => _injector = _parent(parent);
+  T get<T>(dynamic token) => injector?.get(token);
+}
